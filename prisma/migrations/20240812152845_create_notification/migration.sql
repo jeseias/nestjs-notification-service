5eee3b2f4068da02ba9cdbc409358f6bdc6f3823
jeseias <domingosjeseias@gmail.com>
2024-08-12 16:28:58 +0100

    feat: add notification table

---
+++ prisma/migrations/20240812152845_create_notification/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "Notication" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "recipientId" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "category" TEXT NOT NULL,
+    "readAt" DATETIME,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateIndex
+CREATE INDEX "Notication_recipientId_idx" ON "Notication"("recipientId");
